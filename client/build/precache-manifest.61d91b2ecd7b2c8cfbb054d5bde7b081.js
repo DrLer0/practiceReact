13b6578dd9279613e740f7e099eed640d2321e44
Jonathan Doctolero <jonathan.doctolero7@gmail.com>
2020-04-18 11:19:05 -0700

    edited server.js and client/build

---
+++ client/build/precache-manifest.61d91b2ecd7b2c8cfbb054d5bde7b081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a903325301930ad39a1b",
+    "url": "/static/css/main.15179767.chunk.css"
+  },
+  {
+    "revision": "a903325301930ad39a1b",
+    "url": "/static/js/main.23345be4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "219f29db5f10382fa35f",
+    "url": "/static/js/2.600514d9.chunk.js"
+  },
+  {
+    "revision": "860bbf258868b6011b53f77f24719b9a",
+    "url": "/index.html"
+  }
+];
